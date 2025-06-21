3d2f9161a1b7ba3775119a36c96d5135eac78e6e
Chris Phillips <mando.aka.chris@gmail.com>
2025-06-21 11:02:11 -0700

    Cursor fixed php type confusion (yuck)

--- tests/Feature/Goals/APIGoalGetTest.php
+++ tests/Feature/Goals/APIGoalGetTest.php
@@ -8,14 +8,18 @@ use Laravel\Passport\Passport;
 uses(RefreshDatabase::class);
 
 test('Goal get Active endpoint should return a list of active goals for the current user', function () {
-    $user = User::factory()->create();
+    // Arrange
+    /** @var \App\Models\User $user */
+    $user = User::factory()->createOne();
     Passport::actingAs($user);
 
     Goal::factory()->count(3)->create(['user_id' => $user->id, 'status' => 'ACTIVE']);
     Goal::factory()->count(2)->create(['user_id' => $user->id, 'status' => 'OPEN']);
 
+    // Act
     $response = $this->getJson('/api/goals/active');
 
+    // Assert
     $response->assertStatus(200);
     $response->assertJsonCount(3);
 });
